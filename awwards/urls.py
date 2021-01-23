626a5ea04992495079ef266309084e6f52ed5dee
fiona <fionaniwe@gmail.com>
2021-01-23 22:48:19 +0200

    static folder is created

--- awwards/urls.py
+++ awwards/urls.py
@@ -18,6 +18,8 @@ from django.contrib.auth import views
 from django.contrib import admin
 from django.contrib.auth import views
 
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('projects.urls')),
