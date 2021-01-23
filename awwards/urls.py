6032151c80940bd73c6f70738d96d66d1e60ff36
fiona <fionaniwe@gmail.com>
2021-01-23 22:58:42 +0200

    whitenoise is added

--- awwards/urls.py
+++ awwards/urls.py
@@ -19,12 +19,6 @@ from django.contrib import admin
 from django.contrib.auth import views
 
 
-
-
-
-
-
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('projects.urls')),
